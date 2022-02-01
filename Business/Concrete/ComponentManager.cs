313e1bbc36f5035336690ccaa305d2a8c7933745
4lbatr0s <serhatoner@protonmail.com>
2022-02-01 12:23:29 +0300

    update componentmanager.cs aspects: [SecuredOperations]

--- Business/Concrete/ComponentManager.cs
+++ Business/Concrete/ComponentManager.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Abstract;
+using Business.BusinessAspects.Autofac;
 using Business.Constants;
 using Core.Utilities.Result;
 using DataAccess.Abstract;
@@ -16,44 +17,45 @@ namespace Business.Concrete
             _componentDal = componentDal;
          }
 
+        [SecuredOperations("component.delete, admin")]
         public IResult Delete(Component component)
         {
             _componentDal.Delete(component);
             return new SuccessResult(Message.ComponentDeleted);
         }
 
-
-        public IDataResult<List<Component>> GetAll()
+         public IDataResult<List<Component>> GetAll()
         {
             var result = _componentDal.GetAll();
             return new SuccessDataResult<List<Component>>(result);
         }
 
-        public IDataResult<List<Component>> GetAllByModel(string modelName)
+         public IDataResult<List<Component>> GetAllByModel(string modelName)
         {
             var result = _componentDal.GetAll(c => c.ModelName == modelName);
             return new SuccessDataResult<List<Component>>(result);
         }
 
-        public IDataResult<Component> GetById(int id)
+         public IDataResult<Component> GetById(int id)
         {
             var result = _componentDal.Get(c => c.Id == id);
             return new SuccessDataResult<Component>(result);
         }
 
+        [SecuredOperations("component.update, admin")]
         public IResult Update(Component component)
         {
             _componentDal.Update(component);
             return new SuccessResult(Message.ComponentUpdated);
         }
 
-      
-        public IDataResult<List<Component>> GetByBrand(int brandId)
+         public IDataResult<List<Component>> GetByBrand(int brandId)
         {
             var result = _componentDal.GetAll(b => b.BrandId == brandId);
             return new SuccessDataResult<List<Component>>(result);
         }
 
+        [SecuredOperations("component.add, admin")]
         public IResult Add(Component component)
         {
             _componentDal.Add(component);
